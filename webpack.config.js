9861bbea082d1c7b37eb80b52a9012ce29e099b6
ali <mdali2016.227@gmail.com>
2022-06-03 23:13:51 +0600

    [Add] webpack setup with sass config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: "development",
+    entry: [
+        './src/app.js',
+        './src/style.scss'
+    ],
+    output: {
+        filename: 'application.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
+
+}
