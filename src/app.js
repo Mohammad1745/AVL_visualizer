332c508a4b4b1f78c3b7166535e9dd3a9f215c60
ali <mdali2016.227@gmail.com>
2022-06-04 00:43:22 +0600

    [Add] range slider input

--- src/app.js
+++ src/app.js
@@ -19,4 +19,13 @@ function loadHeaderContents() {
         <button id="header_visualize_btn" class="header__visualize-btn">${visualizerButtonText}</button>
         <div id="header_range_slider" class="header__range-slider"></div>
     `
+    loadRangeSlider()
+}
+
+function loadRangeSlider() {
+    let rangeSlider = document.getElementById('header_range_slider')
+    rangeSlider.innerHTML = `
+        <div id="slider_title" class="header__range-slider__title">Size & Speed</div>
+        <input type="range" min="5" max="30" value="10"  id="slider_input" class="header__range-slider__input">
+    `
 }
