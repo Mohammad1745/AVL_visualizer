cce6c3f462bf7402019fb71f82b656dcae1cd68c
ali <mdali2016.227@gmail.com>
2022-06-04 17:45:22 +0600

    [Add] separated ui generation file

--- src/app.js
+++ src/app.js
@@ -1,54 +1,6 @@
 import config from "./js/config"
+import loadUi from "./js/ui"
 
 document.addEventListener('DOMContentLoaded', () => {
-    let app = document.getElementById('app')
-    app.innerHTML = `
-        <div id="header" class="header"></div>
-        <div id="subheader" class="subheader"></div>
-        <div id="visualizer" class="visualizer"></div>
-        <div id="footer" class="footer"></div>
-    `
-    loadHeaderContents()
-    loadSubheaderContents()
+    loadUi()
 })
-
-function loadHeaderContents() {
-    let header = document.getElementById('header')
-    header.innerHTML = `
-        <div id="header_brand" class="header__brand">${config.headerBrand}</div>
-        <div id="header_range_slider" class="header__range-slider"></div>
-        <button id="header_visualize_btn" class="header__visualize-btn">${config.visualizerButtonText}</button>
-    `
-    loadRangeSlider()
-}
-
-function loadRangeSlider() {
-    let rangeSlider = document.getElementById('header_range_slider')
-    rangeSlider.innerHTML = `
-        <div id="slider_title" class="header__range-slider__title">Size & Speed</div>
-        <input type="range" min="${config.sliderMin}" max="${config.sliderMax}" value="${config.sliderValue}"  id="slider_input" class="header__range-slider__input">
-    `
-}
-
-function loadSubheaderContents() {
-    let subheader = document.getElementById('subheader')
-    subheader.innerHTML = `        
-        <div id="subheader_tree_options" class="subheader__tree-options"></div>
-        <div id="subheader_keys" class="subheader_keys"></div>
-    `
-    loadTreeOptions()
-}
-
-function loadTreeOptions() {
-    let subheader = document.getElementById('subheader_tree_options')
-    config.trees.map(tree => {
-        let checkBoxInput = `
-            <div class="subheader__checkbox">
-                <input type="checkbox" id="checkbox_input_${tree.key}" name="selected_trees" `+ (config.selectedTrees.includes(tree.key) ? "checked" : '') +`  class="subheader__checkbox__input"/>
-                <span class="subheader__checkbox__option-title" >${tree.title}</span>
-            </div>  
-        `
-        subheader.insertAdjacentHTML('beforeend', checkBoxInput)
-    })
-
-}
